0ab1a9f5be4513896e3835e2b2c5203140ece806
Sabuj <sabujlovescoding@gmail.com>
2020-02-09 19:47:29 +0530

    Pangram Checking.

--- GS/Pangram Checking.cpp
+++ GS/Pangram Checking.cpp
@@ -3,10 +3,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isPan(string str){
+	int alpha[26];
+	memset(alpha,0,sizeof(alpha));
+
+	for(auto x: str){
+		if(x >= 'A' and x <= 'Z')
+			alpha[x - 'A']++;
+		else if(x >= 'a' and x <= 'z')
+			alpha[x - 'a']++;
+	}
+
+	for(int i = 0; i < 26; i++){
+		if(!alpha[i])
+			return false;
+	}
+
+	return true;
+}
+
 int main() {
 	freopen("input.txt", "r", stdin);
 
 	string str = "The quick brown fox jumps over the lazy dog";
+	cout << ((isPan(str)) ? "YES!!!" : "NO!!!") <<endl;
 
 
 	return 0;
